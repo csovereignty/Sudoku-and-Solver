b45e4d8f35cbb5750b7d30c2c9c216dcfec5342e
csovereignty <cbsovereignty@gmail.com>
2021-06-20 21:27:42 -0700

    First commit, grid of 0's created and preemptive versions of "checking" functions made

---
+++ Sudoku.py
@@ -0,0 +1,34 @@
+import random
+
+random.seed()
+
+grid = [[0 for i in range(9)] for j in range(9)]
+
+def create_puzzle(self, grid):
+    possible_numbers = [1,2,3,4,5,6,7,8,9]
+
+def check_row(self, grid, row, number):
+    #check if row is valid
+    if number in grid[row]:
+        return True
+    return False
+
+def check_col(self, grid, col, number):
+    #check if col is valid
+    for i in range(9):
+        if grid[i][col] == number:
+            return True
+    return False
+
+def check_subgrid(self,grid,row,col,number):
+		#check subgrid for valid numbers
+		sub_row = (row // 3) * 3
+		sub_col = (col // 3)  * 3
+		for i in range(sub_row, (sub_row + 3)): 
+			for j in range(sub_col, (sub_col + 3)): 
+				if grid[i][j] == number: 
+					return True
+		return False
+
+
+print(grid)
