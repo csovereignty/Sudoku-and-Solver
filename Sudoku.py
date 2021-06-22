44604bfb0d5799f349c1fce8107b533af0229460
csovereignty <christopher.bowenx@gmail.com>
2021-06-21 20:22:22 -0700

    Just saving progress for laptop access

--- Sudoku.py
+++ Sudoku.py
@@ -2,33 +2,21 @@ import random
 
 random.seed()
 
+#creates our grid which is basically a set of lists inside of a list
 grid = [[0 for i in range(9)] for j in range(9)]
 
+#prints our grid in a legible format! Yay!
+for s in grid:
+    print(*s)
+print('')
+
+for i in range(9):
+    if grid[0][i] == 0:
+        grid[0][i] = random.randrange(1,9,1)
+    for j in grid[0]:
+        if grid[0][j] == grid[0][i]:
+            continue #holdover here
 
 
-def check_row(self, grid, row, number):
-    #check if row is valid
-    if number in grid[row]:
-        return True
-    return False
-
-def check_col(self, grid, col, number):
-    #check if col is valid
-    for i in range(9):
-        if grid[i][col] == number:
-            return True
-    return False
-
-def check_subgrid(self,grid,row,col,number):
-		#check subgrid for valid numbers
-		sub_row = (row // 3) * 3
-		sub_col = (col // 3)  * 3
-		for i in range(sub_row, (sub_row + 3)): 
-			for j in range(sub_col, (sub_col + 3)): 
-				if grid[i][j] == number: 
-					return True
-		return False
-
-
-print(grid)
-#finish working on making the grid and creating a proper sudoku puzzle then work on solving
+for s in grid:
+    print(*s)
--- tempcode.py
+++ tempcode.py
@@ -23,3 +23,26 @@ def create_puzzle(self, grid):
 		    break
     grid[row][col]=0  
     return False
+
+def check_row(self, grid, row, number):
+    #check if row is valid
+    if number in grid[row]:
+        return True
+    return False
+
+def check_col(self, grid, col, number):
+    #check if col is valid
+    for i in range(9):
+        if grid[i][col] == number:
+            return True
+    return False
+
+def check_subgrid(self,grid,row,col,number):
+		#check subgrid for valid numbers
+		sub_row = (row // 3) * 3
+		sub_col = (col // 3)  * 3
+		for i in range(sub_row, (sub_row + 3)): 
+			for j in range(sub_col, (sub_col + 3)): 
+				if grid[i][j] == number: 
+					return True
+		return False
